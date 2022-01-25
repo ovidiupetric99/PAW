45500a81aa3b0496cec845d824015cbad0b24123
Ovidiu <ovidiupetric99@gmail.com>
2022-01-25 19:54:08 +0200

    Primele 3 laboratoare

---
+++ Laborator_3/classic/index.php
@@ -0,0 +1,75 @@
+<?php  include('code.php'); ?>
+<?php 
+	if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$update = true;
+		$record = mysqli_query($db, "SELECT * FROM info WHERE id=$id");
+
+		if (count((array)$record) == 1 ) {
+			$n = mysqli_fetch_array($record);
+			$name = $n['name'];
+			$email = $n['email'];
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php if (isset($_SESSION['message'])): ?>
+	<div class="msg">
+		<?php 
+			echo $_SESSION['message']; 
+			unset($_SESSION['message']);
+		?>
+	</div>
+<?php endif ?>
+
+<?php $results = mysqli_query($db, "SELECT * FROM info"); ?>
+
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Email</th>
+			<th colspan="2">Action</th>
+		</tr>
+	</thead>
+	
+	<?php while ($row = mysqli_fetch_array($results)) { ?>
+		<tr>
+			<td><?php echo $row['name']; ?></td>
+			<td><?php echo $row['email']; ?></td>
+			<td>
+				<a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
+			</td>
+			<td>
+				<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+			</td>
+		</tr>
+	<?php } ?>
+</table>
+
+	<form method="post" action="code.php" >
+    <input type="hidden" name="id" value="<?php echo $id; ?>">
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name" value="<?php echo $name; ?>">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="text" id="name" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+        <?php if ($update == true): ?>
+	<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
+<?php else: ?>
+	<button class="btn" type="submit" name="save" >Save</button>
+<?php endif ?>
+		</div>
+	</form>
+</body>
+</html>
